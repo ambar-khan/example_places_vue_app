26bd57a589285c776fb0459593ca9c0dfaa76be3
Ambar <ambarmogg@me.com>
2020-12-14 18:45:00 -0800

    add index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places">
+      {{ place.name }}
+      </div>
   </div>
 </template>
 
@@ -8,13 +11,27 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Places!",
+      places: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    console.log("in created");
+    this.placesIndex();
+  },
+  methods: {
+    placesIndex: function () {
+      console.log("products index..");
+      axios.get("/api/places").then((response) => {
+        console.log(response.data);
+        this.places = response.data;
+      });
+    },
+  },
 };
 </script>
